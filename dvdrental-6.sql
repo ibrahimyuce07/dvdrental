cc6b194598bddc0d74f3a77134e48032c9f918be
ibrahim.yuce <ibrahimyucee@outlook.com>
2022-01-14 23:30:08 +0300

    odev6

---
+++ dvdrental-6.sql
@@ -0,0 +1,13 @@
+--Merhabalar,
+--
+--Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+--
+--film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+select avg(rental_rate) from film;
+--film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
+select count(title) from film where title like 'C%';
+--film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+select max(length) from film where rental_rate = 0.99;
+--film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+select count(distinct replacement_cost) from film where length > 150;
+--Kolay Gelsin.
